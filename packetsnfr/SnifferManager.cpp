993f0307d6732d2ec7239c7d48947cbe2887d59f
Boyuan Zhang <zhang_boyuan@hotmail.com>
2014-01-21 17:07:11 -0500

    Updated management options

--- packetsnfr/SnifferManager.cpp
+++ packetsnfr/SnifferManager.cpp
@@ -22,17 +22,24 @@ bool SnifferManager::Initialize()
 //Validates if the request device to be listened on exists, if it does then listen to all traffic on that device
 bool SnifferManager::OpenDevice(int deviceIndex)
 {	
-	if( deviceIndex > 0 && deviceIndex <= m_sniffer->GetDeviceCount() )
+	if( validateDevice( deviceIndex) )
 	{
-		cout << "About to start listening in on device: " << deviceIndex << endl;
+		cout << "About to start listening in on device: " << deviceIndex << endl << endl;
+		cout << "Please press esc at any time to stop listening." << endl;
 		
 		//decrement the deviceIndex by 1 because the packet sniffer uses zero indexing
 		return m_sniffer->OpenDevice(deviceIndex-1);
 	}
-	else
-		cout << "Invalid entry, index entered is out of range!" << endl;
 
 	return false;
+}
+
+void SnifferManager::DisplayDeviceInformation( int deviceIndex )
+{
+	if( validateDevice( deviceIndex ) )
+	{
+		m_sniffer->DisplayDeviceInformation( deviceIndex-1);
+	}
 }
 
 bool SnifferManager::GetPacket()
@@ -46,4 +53,15 @@ bool SnifferManager::GetPacket()
 void SnifferManager::DisplayDevices()
 {
 	m_sniffer->DisplayAllDevices();
+}
+
+bool SnifferManager::validateDevice(int deviceIndex)
+{
+	if( deviceIndex > 0 && deviceIndex <= m_sniffer->GetDeviceCount() )
+		return true;
+	else
+	{
+		cout << "Invalid entry, index entered is out of range!" << endl;
+		return false;
+	}
 }
