8fd8b3ae432b08ba8b7bca9ebec0743e011e32ff
Boyuan Zhang <zhang_boyuan@hotmail.com>
2014-01-21 17:07:57 -0500

    Added advanced device information retrieval

--- packetsnfr/PacketSniffer.cpp
+++ packetsnfr/PacketSniffer.cpp
@@ -1,10 +1,9 @@
 #include "PacketSniffer.h"
-
-//Eventually all standard output statements should be headed to an output class, but for now I'll just stick 
 //the outputs in here
 using namespace std;
 
 const int PacketSniffer::MAX_PACKET_SIZE = 65536;
+const short PacketSniffer::IP_LENGTH = 16;
 
 PacketSniffer::PacketSniffer ()
 {
@@ -133,3 +132,86 @@ void PacketSniffer::DisplayDevices( pcap_if_t* device, int deviceNumber)
 	if( device->next != NULL )
 		DisplayDevices( device->next, deviceNumber+1 );
 }
+
+void PacketSniffer::DisplayDeviceInformation( int deviceNumber )
+{
+	m_device = GetDevice( deviceNumber);
+
+	if( m_device )
+	{
+		//Pointer to the list of addresses
+		pcap_addr_t *address;
+
+		cout << "Device name: " << m_device->name << endl;
+		cout << "Loopback Address: ";
+		if( (m_device->flags & PCAP_IF_LOOPBACK) == 0 )
+			cout << "no" << endl;
+		else
+			cout << "yes" << endl;
+		for( address = m_device->addresses; address; address = address->next )
+		{
+			switch( address->addr->sa_family )
+			{
+				//IPV4 family, note destination address might be null if device interface isn't a point-to-point interface
+				case AF_INET:
+					cout << "Address Family Name: AF_INET" << endl;
+					if( address->addr)
+						cout << "Address: " << iptostr( ((struct sockaddr_in*)address->addr)->sin_addr.S_un.S_addr) << endl;
+					if( address->netmask)
+						cout << "Netmask: " << iptostr( ((struct sockaddr_in*)address->netmask)->sin_addr.S_un.S_addr) << endl;
+					if( address->broadaddr)
+						cout << "Broadcast Address: " << iptostr( ((struct sockaddr_in*)address->broadaddr)->sin_addr.S_un.S_addr) << endl;
+					if( address->dstaddr)
+						cout << "Destination Address: " << iptostr( ((struct sockaddr_in*)address->dstaddr)->sin_addr.S_un.S_addr) << endl;
+					break;
+				//IPV6 family
+				case AF_INET6:
+					char ip6str[128];
+					cout << "Address Family Name: AF_INET6" << endl;
+					if( address->addr )
+						cout << "Address: " << ip6tostr( address->addr, ip6str, sizeof( ip6str ) ) << endl;
+					break;
+				default:
+					cout << "Address family unknown";
+					break;
+			}
+		}
+	}
+}
+
+char* PacketSniffer::iptostr( u_long ip)
+{
+    static char output[IP_LENGTH];
+    u_char *p;
+
+	//ip address converted to an unsigned character pointer gives the network address
+	//in the first four buckets
+    p = (u_char *)&ip;
+    sprintf_s(output, "%d.%d.%d.%d", p[0], p[1], p[2], p[3]);
+
+	return output;
+}
+
+//code here taken from winpcap.org for the conversion from an ipv6 address to a string
+//this uses the Ws2_32.lib, so the library must be added to additional dependencies in the project
+char* PacketSniffer::ip6tostr(struct sockaddr *sockaddr, char *address, int addrlen)
+{
+    socklen_t sockaddrlen;
+
+    #ifdef WIN32
+    sockaddrlen = sizeof(struct sockaddr_in6);
+    #else
+    sockaddrlen = sizeof(struct sockaddr_storage);
+    #endif
+
+    if(getnameinfo(sockaddr, 
+        sockaddrlen, 
+        address, 
+        addrlen, 
+        NULL, 
+        0, 
+        NI_NUMERICHOST) != 0) address = "getnameinfo in ip6tostr failed";
+
+    return address;
+}
+
