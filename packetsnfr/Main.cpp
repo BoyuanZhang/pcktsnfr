0165147accf40e90fdabcddc4b3722720bddc4df
Boyuan Zhang <zhang_boyuan@hotmail.com>
2014-01-21 00:58:09 -0500

    Update Main.cpp

--- packetsnfr/Main.cpp
+++ packetsnfr/Main.cpp
@@ -7,9 +7,10 @@ using namespace std;
 
 int main()
 {
-	//Manages our sniffer manager to provide a layer of abstraction
+	//The client has a handle to the sniffer manager to provide a layer of abstraction
 	SnifferClient *client = new SnifferClient();
-	 //If we were able to get a handle to at least one network adapter, we run the packet sniffer
+	 //If we were able to get a handle to at least one network adapter, we go to the main menu
+	 //on the client
 	if(client->Initialize())
 		client->Menu();
 
