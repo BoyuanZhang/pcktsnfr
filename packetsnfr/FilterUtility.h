a6e12eddc3028042dc818093898dab8b9693a938
Boyuan Zhang <zhang_boyuan@hotmail.com>
2014-01-22 23:35:53 -0500

    Future use for easy filter expressions

---
+++ packetsnfr/FilterUtility.h
@@ -0,0 +1,28 @@
+//use an include to avoid PCH warning
+#include <string.h> 
+
+namespace FilterUtility
+{
+	//default filter
+	const char* filter_IP4TCP = "ip and tcp";
+
+	void filter_TCPONPORT( char* port, char* result )
+	{
+		char* filter_TCPSRCPORT = "tcp src port ";
+		//append the tcp src port filter onto the result
+		for( int i = 0; i< (int)strlen(filter_TCPSRCPORT); i++)
+		{
+			*result = filter_TCPSRCPORT[i];
+			result++;
+		}
+		//append the actual port to be filtered on to the result
+		for( int i = 0; i< (int)strlen(port); i++)
+		{
+			*result = port[i];
+			result++;
+		}
+
+		//null terminate filter
+		*result = '\0';
+	}
+}
