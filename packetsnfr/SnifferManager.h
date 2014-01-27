a57dca506dc86d58def029ea259ad81564881fa1
Boyuan Zhang <zhang_boyuan@hotmail.com>
2014-01-27 04:58:02 -0500

    Updated to store total bandwidth and toggle recording

--- packetsnfr/SnifferManager.h
+++ packetsnfr/SnifferManager.h
@@ -4,14 +4,17 @@ class SnifferManager {
 	private:
 		//Handle to the packet sniffer that this class manages
 		PacketSniffer *m_sniffer;
-
+		bool m_recordBandwidth;
 		bool validateDevice(int deviceIndex);
+		//Total bandwidth in KB
+		double m_totalBandwidth; 
 	public:
 		SnifferManager();
 		~SnifferManager();
 
 		//Tries to initialize the packet sniffer, returns false if an error occurs
 		bool Initialize();
+		void ToggleRecord();
 		//Tell the packet sniffer to listen in on this device index, if an exception occurs return the given error code
 		bool GetPacket();
 		bool OpenDevice( int deviceIndex );
